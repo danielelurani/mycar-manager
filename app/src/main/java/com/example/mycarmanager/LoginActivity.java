b3ca9a16b54b2e57b8d57a51b161ca3efd63a0fd
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-08-24 12:29:32 +0200

    Modifiche alle activity Login e Registration

--- app/src/main/java/com/example/mycarmanager/LoginActivity.java
+++ app/src/main/java/com/example/mycarmanager/LoginActivity.java
@@ -2,13 +2,38 @@ package com.example.mycarmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView signupLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signupLink = findViewById(R.id.signupLink);
+
+        // Quando si clicca sull'apposito link si viene mandati alla pagina di registrazione
+        signupLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent goToSignupPage;
+                goToSignupPage = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(goToSignupPage);
+            }
+        });
+    }
+
+    // finishaffinity rimuove le connessioni con lo stack, finish chiude l'applicazione
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+        finish();
     }
 }
--- app/src/main/java/com/example/mycarmanager/RegistrationActivity.java
+++ app/src/main/java/com/example/mycarmanager/RegistrationActivity.java
@@ -2,13 +2,41 @@ package com.example.mycarmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private TextView loginLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        loginLink = findViewById(R.id.loginLink);
+
+        // Quando si clicca sull'apposito link si viene mandati alla pagina di login
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                goToLogin();
+            }
+        });
+    }
+
+    private void goToLogin(){
+        Intent goToLoginPage;
+        goToLoginPage = new Intent(RegistrationActivity.this, LoginActivity.class);
+        startActivity(goToLoginPage);
+        RegistrationActivity.this.finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        goToLogin();
     }
 }
