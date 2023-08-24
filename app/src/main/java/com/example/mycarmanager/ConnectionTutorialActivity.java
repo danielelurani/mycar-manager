36789feabfe61295ab497b8d787522c8fca0b040
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-08-24 14:03:30 +0200

    Implementata activity con istruzioni
    Implementata activity con prima parte di istruzioni per la connessione all'auto

---
+++ app/src/main/java/com/example/mycarmanager/ConnectionTutorialActivity.java
@@ -0,0 +1,66 @@
+package com.example.mycarmanager;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ConnectionTutorialActivity extends AppCompatActivity {
+
+    Button tutorialEndButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection_tutorial);
+
+        tutorialEndButton = findViewById(R.id.tutorialEndButton);
+
+        tutorialEndButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // controllo se il bluetooth è acceso
+                // in caso negativo avvisa con un alert
+                // in caso positivo continua alla prossima activity
+                if(!checkBluetooth()){
+
+                    AlertDialog alertDialog = new AlertDialog.Builder(ConnectionTutorialActivity.this, R.style.TutorialAlertDialogStyle).create();
+                    alertDialog.setTitle("BLUETOOTH IS OFF!");
+                    alertDialog.setMessage("Turn Bluetooth ON in order to continue!");
+                    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                } else {
+
+                    Intent goToDeviceSelectionActivity;
+
+                    goToDeviceSelectionActivity = new Intent(ConnectionTutorialActivity.this, DeviceSelectionActivity.class);
+                    startActivity(goToDeviceSelectionActivity);
+                    ConnectionTutorialActivity.this.finish();
+                }
+            }
+        });
+    }
+
+    // metodo che controlla se il bluetooth è acceso
+    private boolean checkBluetooth(){
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            return false;
+        } else if (!mBluetoothAdapter.isEnabled()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
--- app/src/main/java/com/example/mycarmanager/RegistrationActivity.java
+++ app/src/main/java/com/example/mycarmanager/RegistrationActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private TextView loginLink;
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,7 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         loginLink = findViewById(R.id.loginLink);
+        signupButton = findViewById(R.id.signupButton);
 
         // Quando si clicca sull'apposito link si viene mandati alla pagina di login
         loginLink.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +27,19 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 goToLogin();
+            }
+        });
+
+        // Quando ci si registra si viene mandati alla pagina di prima connessione all'auto
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent goToConnectionTutorial;
+
+                goToConnectionTutorial = new Intent(RegistrationActivity.this, ConnectionTutorialActivity.class);
+                startActivity(goToConnectionTutorial);
+                RegistrationActivity.this.finish();
             }
         });
     }
