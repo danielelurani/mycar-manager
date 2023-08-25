976ceda9d8ee4e36cde13418cdfaa98555424f95
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-08-25 12:35:22 +0200

    Modificato design Bluetooth altert dialog
    Modificato il design dell'alert dialog che segnala che il bluetooth è spento

--- app/src/main/java/com/example/mycarmanager/ConnectionTutorialActivity.java
+++ app/src/main/java/com/example/mycarmanager/ConnectionTutorialActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +41,7 @@ public class ConnectionTutorialActivity extends AppCompatActivity {
                                     dialogInterface.dismiss();
                                 }
                             });
+                    alertDialog.getWindow().setBackgroundDrawableResource(R.drawable.bluetooth_off_dialog);
                     alertDialog.show();
                 } else {
 
