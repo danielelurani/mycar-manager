2409077a21df3a38061a81cdfe908a575515dda5
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-08-25 18:31:08 +0200

    Aggiunta icone + test design
    Aggiunte alcune icone ed implementato un design di test per la schermata delle caratteristiche dell'auto.

--- app/src/main/java/com/example/mycarmanager/Test.java
+++ app/src/main/java/com/example/mycarmanager/Test.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TestDesignActivity extends AppCompatActivity {
+public class Test extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_design);
+        setContentView(R.layout.features_test_design_v2);
     }
 }
