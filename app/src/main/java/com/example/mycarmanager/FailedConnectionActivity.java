b9cc350874a4af6a9461ef7465bea07355252827
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-08-25 12:22:39 +0200

    Implementata FailedConnectionActivity
    Implementata activity che segnala il fallimento della connessione all'auto.

--- app/src/main/java/com/example/mycarmanager/FailedConnectionActivity.java
+++ app/src/main/java/com/example/mycarmanager/FailedConnectionActivity.java
@@ -2,13 +2,30 @@ package com.example.mycarmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FailedConnectionActivity extends AppCompatActivity {
+
+    private Button tryAgainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_failed_connection);
+
+        tryAgainButton = findViewById(R.id.tryagainButton);
+
+        tryAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnToDeviceSelection;
+                returnToDeviceSelection = new Intent(FailedConnectionActivity.this, DeviceSelectionActivity.class);
+                startActivity(returnToDeviceSelection);
+                FailedConnectionActivity.this.finish();
+            }
+        });
     }
 }
