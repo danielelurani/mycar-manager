42c9fbd8fb406fe83a7b689e4bbd6000d8d0b4ef
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-08-26 15:37:17 +0200

    Implementata posizione auto in CarMapActivity

--- app/src/main/java/com/example/mycarmanager/CarMapActivity.java
+++ app/src/main/java/com/example/mycarmanager/CarMapActivity.java
@@ -3,16 +3,20 @@ package com.example.mycarmanager;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class CarMapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.map_test_design);
+        setContentView(R.layout.activity_car_map);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapActual);
         mapFragment.getMapAsync(this);
@@ -22,5 +26,12 @@ public class CarMapActivity extends AppCompatActivity implements OnMapReadyCallb
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
+        LatLng position = new LatLng(39.222334, 9.114042);
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(position)
+                .title("Your Car"));
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 16));
     }
 }
