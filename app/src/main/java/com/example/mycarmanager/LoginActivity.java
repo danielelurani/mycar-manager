6219143afe982a9463fbb57fc41e4dbd642265d2
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-08-26 13:17:42 +0200

    Implementato TEST BUTTON
    Implementato un pulsante di testing per passare direttamente alle activity da testare.

--- app/src/main/java/com/example/mycarmanager/LoginActivity.java
+++ app/src/main/java/com/example/mycarmanager/LoginActivity.java
@@ -1,15 +1,16 @@
 package com.example.mycarmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView signupLink;
+    private Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         signupLink = findViewById(R.id.signupLink);
+        testButton = findViewById(R.id.testButton);
 
         // Quando si clicca sull'apposito link si viene mandati alla pagina di registrazione
         signupLink.setOnClickListener(new View.OnClickListener() {
@@ -24,8 +26,18 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 Intent goToSignupPage;
-                goToSignupPage = new Intent(LoginActivity.this, CarMapActivity.class);
+                goToSignupPage = new Intent(LoginActivity.this, RegistrationActivity.class);
                 startActivity(goToSignupPage);
+            }
+        });
+
+        // BOTTONE DI TEST PER TESTARE IMMEDIATAMENTE LE NUOVE ACTIVITY
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent testIntent;
+                testIntent = new Intent(LoginActivity.this, CarMapActivity.class);
+                startActivity(testIntent);
             }
         });
     }
