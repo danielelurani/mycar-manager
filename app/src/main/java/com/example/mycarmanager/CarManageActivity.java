43a83c3754b686fdaf9ddb2aa0e99d0409bcd6a8
RichSenes <richsenes@gmail.com>
2023-08-26 20:13:43 +0200

    Car Manage Activity
    Creazione ed inserimento degli elementi della activity Manage per la gestione delle impostazioni delle auto. Creazione del layout "layout_border_v3" in modo da avere un bordo più spesso da utilizzare.

---
+++ app/src/main/java/com/example/mycarmanager/CarManageActivity.java
@@ -0,0 +1,14 @@
+package com.example.mycarmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_car_manage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_manage);
+    }
+}
