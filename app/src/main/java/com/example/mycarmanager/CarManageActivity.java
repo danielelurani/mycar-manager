ce2d9a3ebaf64a7deede35131e69c95d734175bb
RichSenes <richsenes@gmail.com>
2023-08-27 18:03:03 +0200

    Car Manage & Garage Activity
    Modifiche strutturali alle pagine xml "Garage" e "Manage" in modo da inserire tutte le immagini e loghi che dovranno comparire all'interno di ciascuna di esse. Iniziato inserimento di id appropriati nella pagine (da completare).

--- app/src/main/java/com/example/mycarmanager/CarManageActivity.java
+++ app/src/main/java/com/example/mycarmanager/CarManageActivity.java
@@ -3,11 +3,62 @@ package com.example.mycarmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
 
 public class CarManageActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_manage);
     }
+
+    /*
+    PARTE DA IMPLEMENTARE SUCCESSIVAMENTE
+
+    int immaginiTotali = 5, contatoreImmagini = 0;
+    ImageView immagini[] = {(ImageView) findViewById(R.id.manage_image1),
+            (ImageView) findViewById(R.id.manage_image2),
+            (ImageView) findViewById(R.id.manage_image3),
+            (ImageView) findViewById(R.id.manage_image4),
+            (ImageView) findViewById(R.id.manage_image5),
+            (ImageView) findViewById(R.id.manage_image6) };
+
+    protected void leftArrowClick () {
+        if (contatoreImmagini != 0) { contatoreImmagini--; aggiornaImmagine(contatoreImmagini); }
+        else {contatoreImmagini = 5; aggiornaImmagine(contatoreImmagini); }
+    }
+
+    protected void rightArrowClick () {
+        if (contatoreImmagini != 5) { contatoreImmagini++; aggiornaImmagine(contatoreImmagini); }
+        else {contatoreImmagini = 0; aggiornaImmagine(contatoreImmagini); }
+    }
+
+    protected void aggiornaImmagine (int numeroImmagine) {
+        modificaImpImmagine(immagini[numeroImmagine], true);
+        for (int i = 0; i < immaginiTotali; i++) {
+            if (i != numeroImmagine) {
+                modificaImpImmagine(immagini[i], false);
+            }
+        }
+    }
+
+    protected void modificaImpImmagine (ImageView immagine, boolean flag) {
+        if (flag) {
+            immagine.setVisibility(ImageView.VISIBLE);
+            immagine.setMinimumHeight(100);
+            immagine.setMinimumWidth(100);
+            immagine.setMaxHeight(100);
+            immagine.setMaxWidth(100);
+        }
+        else {
+            immagine.setVisibility(ImageView.INVISIBLE);
+            immagine.setMinimumHeight(0);
+            immagine.setMinimumWidth(0);
+            immagine.setMaxHeight(0);
+            immagine.setMaxWidth(0);
+        }
+    }
+     */
 }
