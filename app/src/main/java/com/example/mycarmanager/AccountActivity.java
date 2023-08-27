512a329dccbd64ae02aebd9e3c40a3837cca8122
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-08-27 13:16:36 +0200

    Modificato EditProfileDialog

--- app/src/main/java/com/example/mycarmanager/AccountActivity.java
+++ app/src/main/java/com/example/mycarmanager/AccountActivity.java
@@ -1,15 +1,15 @@
 package com.example.mycarmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 public class AccountActivity extends AppCompatActivity {
+
+    private Dialog editProfileDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +29,16 @@ public class AccountActivity extends AppCompatActivity {
 
     public void showDialog(){
 
-        Dialog editProfileDialog = new Dialog(this);
+        editProfileDialog = new Dialog(AccountActivity.this);
         editProfileDialog.setContentView(R.layout.edit_profile_dialog);
+        editProfileDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
 
-        ImageButton closeDialog = findViewById(R.id.closeDialog);
-        Button saveProfile = findViewById(R.id.saveProfile);
+        ImageButton closeDialog = editProfileDialog.findViewById(R.id.closeDialog);
+        Button saveProfile = editProfileDialog.findViewById(R.id.saveProfile);
 
         saveProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
                 editProfileDialog.dismiss();
             }
         });
