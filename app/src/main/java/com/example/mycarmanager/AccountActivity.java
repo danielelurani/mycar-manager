ae63d6418ddf62f1361139e26915537eb69572f9
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-08-27 17:03:19 +0200

    Implementato AlertsDialog
    Implementato Dialog che spunta fuori quando si clicca l'icona delle notifiche. Esso mostra vari tipi di notifiche riguardanti l'auto.

--- app/src/main/java/com/example/mycarmanager/AccountActivity.java
+++ app/src/main/java/com/example/mycarmanager/AccountActivity.java
@@ -3,13 +3,16 @@ package com.example.mycarmanager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class AccountActivity extends AppCompatActivity {
 
-    private Dialog editProfileDialog;
+    private Dialog editProfileDialog, alertsDialog;
+    private LinearLayout alertIconLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +20,47 @@ public class AccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_account);
 
         Button editProfileButton = findViewById(R.id.editProfileButton);
+        alertIconLayout = findViewById(R.id.alertsIcon);
 
         editProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                showDialog();
+                showEditProfileDialog();
+            }
+        });
+
+        alertIconLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showAlertsDialog();
             }
         });
     }
 
-    public void showDialog(){
+    public void showAlertsDialog(){
+
+        alertsDialog = new Dialog(AccountActivity.this);
+        alertsDialog.setContentView(R.layout.alerts_dialog);
+        alertsDialog.getWindow().getAttributes().windowAnimations = R.style.AlertsDialogAnimation;
+        alertsDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        alertsDialog.getWindow().getAttributes().gravity = Gravity.TOP;
+
+        ImageButton closeDialog = alertsDialog.findViewById(R.id.closeDialog);
+
+        closeDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                alertsDialog.dismiss();
+            }
+        });
+
+        alertsDialog.show();
+    }
+
+    public void showEditProfileDialog(){
 
         editProfileDialog = new Dialog(AccountActivity.this);
         editProfileDialog.setContentView(R.layout.edit_profile_dialog);
