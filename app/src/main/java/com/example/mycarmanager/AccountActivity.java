d5f50ae39b188b0ddc9dc3122dfc3cdc8c895c5e
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-08-29 16:18:02 +0200

    Implementata barra di navigazione laterale

--- app/src/main/java/com/example/mycarmanager/AccountActivity.java
+++ app/src/main/java/com/example/mycarmanager/AccountActivity.java
@@ -1,18 +1,35 @@
 package com.example.mycarmanager;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.app.Dialog;
+import android.content.ClipData;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class AccountActivity extends AppCompatActivity {
 
     private Dialog editProfileDialog, alertsDialog;
-    private LinearLayout alertIconLayout;
+    private LinearLayout alertIconLayout, navMenuButton;
+    private DrawerLayout drawerLayout;
+    private NavigationView navMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,18 @@ public class AccountActivity extends AppCompatActivity {
 
         Button editProfileButton = findViewById(R.id.editProfileButton);
         alertIconLayout = findViewById(R.id.alertsIcon);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navMenuButton = findViewById(R.id.navMenuButton);
+        navMenu = findViewById(R.id.nav_view);
+
+        navMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                drawerLayout.openDrawer(GravityCompat.START);
+                navMenu.bringToFront();
+            }
+        });
 
         editProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +93,8 @@ public class AccountActivity extends AppCompatActivity {
 
         editProfileDialog = new Dialog(AccountActivity.this);
         editProfileDialog.setContentView(R.layout.edit_profile_dialog);
+        editProfileDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
         editProfileDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
 
         ImageButton closeDialog = editProfileDialog.findViewById(R.id.closeDialog);
