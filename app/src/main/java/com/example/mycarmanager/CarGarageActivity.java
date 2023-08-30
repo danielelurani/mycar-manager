f9a10bd34a742eef9c561800df672eab60c66993
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-08-30 17:26:18 +0200

    Iniziata implementato logica

--- app/src/main/java/com/example/mycarmanager/CarGarageActivity.java
+++ app/src/main/java/com/example/mycarmanager/CarGarageActivity.java
@@ -1,14 +1,111 @@
 package com.example.mycarmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import static com.example.mycarmanager.User.users;
+import static com.example.mycarmanager.LoginActivity.USER_EXTRA;
+
+import com.google.android.material.navigation.NavigationView;
+
+import org.w3c.dom.Text;
+
+import java.io.Serializable;
 
 public class CarGarageActivity extends AppCompatActivity {
+
+    private User user;
+    private TextView navBarUsername;
+    private Dialog alertsDialog;
+    private LinearLayout alertIconLayout, navMenuButton;
+    private DrawerLayout drawerLayout;
+    private NavigationView navMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_garage);
+
+        navBarUsername = findViewById(R.id.navBarUsername);
+        alertIconLayout = findViewById(R.id.alertsIcon);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navMenuButton = findViewById(R.id.navMenuButton);
+        navMenu = findViewById(R.id.nav_view);
+
+        // Prendo tutti gli intent passati a questa activity
+        Intent intentGetter = getIntent();
+
+        Serializable obj = intentGetter.getSerializableExtra(USER_EXTRA);
+
+        // Controllo che l'oggetto passato sia un User. Se non lo è lo istanzio
+        if(obj instanceof User)
+            user = (User) obj;
+        else
+            user = new User();
+
+        // aggiorna tutti i dati da visualizzare correttamente
+        updateData();
+
+        // mostare la barra di navigazione laterale
+        navMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                drawerLayout.openDrawer(GravityCompat.START);
+                navMenu.bringToFront();
+            }
+        });
+
+        // mostra il dialog degli alert
+        alertIconLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showAlertsDialog();
+            }
+        });
+    }
+
+    public void updateData(){
+
+        int numberOfUsers = users.size();
+
+        for (int i = 0; i < numberOfUsers; i++){
+            if(user.getUsername().equals(users.get(i).getUsername()) &&
+                    user.getPassword().equals(users.get(i).getPassword())){
+
+                navBarUsername.setText(users.get(i).getUsername());
+            }
+        }
+    }
+
+    public void showAlertsDialog(){
+
+        alertsDialog = new Dialog(CarGarageActivity.this);
+        alertsDialog.setContentView(R.layout.alerts_dialog);
+        alertsDialog.getWindow().getAttributes().windowAnimations = R.style.AlertsDialogAnimation;
+        alertsDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        alertsDialog.getWindow().getAttributes().gravity = Gravity.TOP;
+
+        ImageButton closeDialog = alertsDialog.findViewById(R.id.closeDialog);
+
+        closeDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                alertsDialog.dismiss();
+            }
+        });
+
+        alertsDialog.show();
     }
 }
--- app/src/main/java/com/example/mycarmanager/LoginActivity.java
+++ app/src/main/java/com/example/mycarmanager/LoginActivity.java
@@ -1,24 +1,57 @@
 package com.example.mycarmanager;
 
+import static com.example.mycarmanager.User.users;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView signupLink;
+    private TextInputEditText usernameLogin, passwordLogin;
+    private User user;
     private Button testButton;
+    private MaterialButton loginButton;
+    public static final String USER_EXTRA = "com.example.mycarmanager.user";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        User loggedUser = new User("user", "user", "user@email.it", "");
+
         signupLink = findViewById(R.id.signupLink);
         testButton = findViewById(R.id.testButton);
+        loginButton = findViewById(R.id.loginButton);
+        usernameLogin = findViewById(R.id.usernameLogin);
+        passwordLogin = findViewById(R.id.passwordLogin);
+
+        // istanzio l'oggetto utente
+        user = new User();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (checkUserData()){
+
+                    updateUserData();
+
+                    Intent goToHomePage;
+                    goToHomePage = new Intent(LoginActivity.this, CarGarageActivity.class);
+                    goToHomePage.putExtra(USER_EXTRA, user);
+                    startActivity(goToHomePage);
+                } else {
+
+                }
+            }
+        });
 
         // Quando si clicca sull'apposito link si viene mandati alla pagina di registrazione
         signupLink.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +73,28 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(testIntent);
             }
         });
+    }
+
+    protected boolean checkUserData(){
+        int numberOfUsers = users.size();
+
+        for (int i = 0; i < numberOfUsers; i++){
+            if(usernameLogin.getText().toString().equals(users.get(i).getUsername()) &&
+                    passwordLogin.getText().toString().equals(users.get(i).getPassword())){
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected void updateUserData(){
+
+        String username = usernameLogin.getText().toString();
+        this.user.setUsername(username);
+
+        String password = passwordLogin.getText().toString();
+        this.user.setPassword(password);
     }
 
     // finishaffinity rimuove le connessioni con lo stack, finish chiude l'applicazione
