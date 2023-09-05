6d53e1cd88e39681401d1b9ed69d124b0a65a556
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-09-05 17:25:23 +0200

    Fix CarMapActivity crash

--- app/src/main/java/com/example/mycarmanager/CarMapActivity.java
+++ app/src/main/java/com/example/mycarmanager/CarMapActivity.java
@@ -2,21 +2,58 @@ package com.example.mycarmanager;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.app.Dialog;
 import android.os.Bundle;
-
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.navigation.NavigationView;
 
 public class CarMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navMenu;
+    private Dialog alertsDialog;
+    private LinearLayout alertIconLayout, navMenuButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_map);
+
+        alertIconLayout = findViewById(R.id.alertsIcon);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navMenuButton = findViewById(R.id.navMenuButton);
+        navMenu = findViewById(R.id.nav_view);
+
+        // mostare la barra di navigazione laterale
+        navMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                drawerLayout.openDrawer(GravityCompat.START);
+                navMenu.bringToFront();
+            }
+        });
+
+        // mostra il dialog degli alert
+        alertIconLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showAlertsDialog();
+            }
+        });
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapActual);
         mapFragment.getMapAsync(this);
@@ -33,5 +70,26 @@ public class CarMapActivity extends AppCompatActivity implements OnMapReadyCallb
                 .title("Your Car"));
 
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 16));
+    }
+
+    public void showAlertsDialog(){
+
+        alertsDialog = new Dialog(CarMapActivity.this);
+        alertsDialog.setContentView(R.layout.alerts_dialog);
+        alertsDialog.getWindow().getAttributes().windowAnimations = R.style.AlertsDialogAnimation;
+        alertsDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        alertsDialog.getWindow().getAttributes().gravity = Gravity.TOP;
+
+        ImageButton closeDialog = alertsDialog.findViewById(R.id.closeDialog);
+
+        closeDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                alertsDialog.dismiss();
+            }
+        });
+
+        alertsDialog.show();
     }
 }
